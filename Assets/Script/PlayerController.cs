412ef39801569299b6844802babc32cbfbf7ca15
Chau Minh Nguyen <cnguyenm@purdue.edu>
2017-10-05 00:23:10 -0400

    prefab, player
    * add player attack
    * get some cave prefab

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -10,6 +10,7 @@ public class PlayerController : MonoBehaviour {
     private Rigidbody rb;
 
     private const string IS_WALKING = "isWalking";
+    private const string TRIGGER_ATTACK = "triggerAttack";
 
 	// Use this for initialization
 	void Start () {
@@ -21,17 +22,29 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+        // get input
         int x = (int)Input.GetAxisRaw("Horizontal");
         int z = (int)Input.GetAxisRaw("Vertical");
 
+        // generate move direction
         Vector3 moveDir = new Vector3(x, 0, z);
 
+        // player look at new direction
         transform.LookAt(transform.position + moveDir);
 
+        // player move
         rb.MovePosition(transform.position + moveDir * velocity * Time.deltaTime);
 
+        // set animator
         bool isWalking = Mathf.Abs(x) > 0 || Mathf.Abs(z) > 0;
         animator.SetBool(IS_WALKING, isWalking);
+
+        // if attack
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            print("Attack");
+            animator.SetTrigger(TRIGGER_ATTACK);
+        }
 	}
 
     
