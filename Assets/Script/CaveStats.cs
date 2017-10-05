61aeb8b7fbb663cebf14bd550a899ad9c2d25222
cnguyenm <cnguyenm@purdue.edu>
2017-10-05 04:11:13 -0400

    Add more * Wumpus * Cave Generation

---
+++ Assets/Script/CaveStats.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class CaveStats {
+
+    public int size; // size: 4x4
+    public float pitProb; // pit Probability
+}
---
+++ Assets/Script/Room.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour {
+
+    // list of direction
+    public Transform leftPos;
+    public Transform rightPos;
+    public Transform upPos;
+    public Transform downPos;
+
+    public enum type
+    {
+        Safe,
+        Pit,
+        Gold,
+        Wumpus
+    }
+}
